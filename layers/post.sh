22a7844707b71c402e9efb00a3ecefdf995b83bd
mitchejj <me>
2023-06-06 02:20:52 -0400

    roll back

--- layers/post.sh
+++ layers/post.sh
@@ -2,7 +2,7 @@
 
 set -ouex pipefail
 
-systemctl enable remote-fs.target
-systemctl enable sshd.socket
-systemctl enable systemd-timesyncd.service
-fc-cache -f /usr/share/fonts
+# systemctl enable remote-fs.target
+# systemctl enable sshd.socket
+# systemctl enable systemd-timesyncd.service
+# fc-cache -f /usr/share/fonts
