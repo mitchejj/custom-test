614227b25d5973b1272d9c08fc5c12613b90121a
mitchejj <me>
2023-06-17 15:39:57 -0400

    the user/group for usbmuxd is not getting installed

--- layers/post.sh.d/usbmuxd.sh
+++ layers/post.sh.d/usbmuxd.sh
@@ -2,9 +2,9 @@
 
 set -ouex pipefail
 
-getent group usbmuxd >/dev/null || groupadd -r usbmuxd -g 113
-getent passwd usbmuxd >/dev/null || \
+echo "adding usbmuxd user/group"
+groupadd -r usbmuxd -g 113
 useradd -r -g usbmuxd -d / -s /sbin/nologin \
 	-c "usbmuxd user" -u 113 usbmuxd
 
-systemctl enable usbmuxd.service
+#systemctl enable usbmuxd.service
