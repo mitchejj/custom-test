3f350ade4b4cc7947e3f9866d105d52c68b980f3
mitchejj <me>
2023-06-28 12:52:15 -0400

    enable rpm-ostreed-automatic.timer

--- layers/post.sh
+++ layers/post.sh
@@ -4,6 +4,7 @@ set -ouex pipefail
 
 systemctl enable flatpak-system-update.timer 
 systemctl enable remote-fs.target
+systemctl enable rpm-ostreed-automatic.timer
 systemctl enable sshd.service
 systemctl enable systemd-timesyncd.service
 fc-cache -f /usr/share/fonts
