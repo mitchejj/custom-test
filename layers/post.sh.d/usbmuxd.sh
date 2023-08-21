3223c1b81eaf4e2d7c77e377e7d8322950b16e03
mitchejj <me>
2023-08-21 14:11:27 -0400

    usbmuxd.sh clean up
      user still not added to build image for some reason (usbmuxd)

--- layers/post.sh.d/usbmuxd.sh
+++ layers/post.sh.d/usbmuxd.sh
@@ -3,8 +3,6 @@
 set -ouex pipefail
 
 echo "adding usbmuxd user/group"
-groupadd -r usbmuxd -g 113
-useradd -r -g usbmuxd -d / -s /sbin/nologin \
-	-c "usbmuxd user" -u 113 usbmuxd
+useradd -r -d / -s /sbin/nologin -c "usbmuxd user" -u 113 usbmuxd
 
-#systemctl enable usbmuxd.service
+systemctl enable usbmuxd.service
