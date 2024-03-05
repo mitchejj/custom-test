ac28eef40663e5ae6ea3dda25c3dd3837ea210eb
mitchejj <me>
2024-03-05 09:24:13 -0500

    The zfs image build 'fix' for a problem that should never exist was flawed.

--- zed/zfs-install.sh
+++ zed/zfs-install.sh
@@ -6,5 +6,5 @@ set -ouex pipefail
 KERNEL="$(rpm -q kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 RELEASE="$(rpm -q kernel --queryformat '-%{RELEASE}')"
 ls -lah /tmp/zfs
-rpm-ostree install /tmp/zfs/*${KERNEL}.rpm
+rpm-ostree install /tmp/zfs/*.rpm
 depmod -A ${KERNEL}
